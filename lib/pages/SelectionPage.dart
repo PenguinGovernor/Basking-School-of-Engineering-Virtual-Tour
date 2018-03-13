d89a420c06b6b1f7de9f47379dee3066d60e6e30
Jorge Henriquez <jorge@TuxMachine.localdomain>
2018-03-12 23:23:54 -0700

    Add pins for all but tim and ce

--- lib/pages/SelectionPage.dart
+++ lib/pages/SelectionPage.dart
@@ -150,7 +150,7 @@ class SelctionPageFab extends StatefulWidget {
 class SelectionPageFabState extends State<SelctionPageFab> {
   MaterialColor fabColor = Colors.blueGrey;
   List checkedItems;
-  var _mapView = new MapView();
+  var mapView = new MapView();
 
   bool isValidSet() {
     for (var i = 0; i < checkedItems.length; i++) {
@@ -161,8 +161,76 @@ class SelectionPageFabState extends State<SelctionPageFab> {
     return false;
   }
 
+  void setMarkers(List items) {
+    mapView.onMapReady.listen((_) {
+      // Always set Mesa
+      mapView.addMarker(
+        new Marker("mesa", "MESA Engineering Program", 37.000258, -122.062930,
+            color: Colors.red),
+      );
+
+      // If computer engineering
+      if (items[majorList.computerEngineering.index]) {
+        mapView.addMarker(new Marker(
+            "mech", "Mechatronics Lab", 37.000189, -122.063545,
+            color: Colors.orange));
+        mapView.addMarker(new Marker(
+            "cnl", "Computer Networks Lab", 37.000271, -122.063057,
+            color: Colors.orange));
+      }
+
+      // If computer science
+      if (items[majorList.computerScience.index]) {
+        mapView.addMarker(new Marker("ll", "Linux Lab", 37.0002225, -122.063148,
+            color: Colors.green));
+
+        mapView.addMarker(new Marker(
+            "cvl", "Computer Vision Lab", 37.000888, -122.063382,
+            color: Colors.green));
+      }
+
+      // If cs gd
+      if (items[majorList.computerSciencGame.index]) {
+        mapView.addMarker(
+            new Marker("gdl", "Game Design Lab", 37.000419, -122.062715, color: Colors.deepPurple));
+      }
+
+    // If ee 
+    if(items[majorList.eletricalEngineering.index]) {
+      mapView.addMarker(
+        new Marker("dl", "DANSER Lab", 37.000929, -122.063308, color: Colors.blue)
+      );
+      mapView.addMarker(
+        new Marker("ee101", "Eletrical Engineering 101 Lab", 37.000358, -122.063413, color: Colors.blue)
+      );
+      mapView.addMarker(
+        new Marker("aol", "Applied Optics Lab", 37.000495, -122.062870, color: Colors.blue)
+      );
+    }
+
+    });
+  }
+
   void showMap() {
-    _mapView.show(new MapOptions(showUserLocation: true));
+    // Show the map
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            initialCameraPosition: new CameraPosition(
+                new Location(37.000369, -122.06323709999998), 18.5),
+            title: "Virtual Tour"),
+        toolbarActions: [new ToolbarAction("Close", 1)]);
+
+    // Set the markers
+    setMarkers(checkedItems);
+
+    // Close when the button is closed
+    mapView.onToolbarAction.listen((id) {
+      if (id == 1) {
+        mapView.dismiss();
+      }
+    });
   }
 
   @override
