80bab71e5736c05ba73e5f9be2fe69da1d83838f
Jorge Henriquez <jorge@TuxMachine.localdomain>
2018-03-14 15:35:54 -0700

    Add mesa description

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 import './pages/HomePage.dart';
 import './pages/SelectionPage.dart';
+import './rooms/Mesa.dart';
 
 void main() {
   MapView.setApiKey("AIzaSyAGPwHFwynyK8RIkLdkZlNpNOS8jAyYexc");
@@ -14,7 +15,8 @@ class BaskinTour extends StatelessWidget {
     return new MaterialApp(
       home: new HomePage(),
       routes: <String, WidgetBuilder>{
-        "/SelectionPage": (BuildContext context) => new SelectionPage()
+        "/SelectionPage": (BuildContext context) => new SelectionPage(),
+        "/MesaPage": (BuildContext context) => new MesaPage()
       },
     );
   }
